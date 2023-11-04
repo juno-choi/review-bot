f462e6bb7ebbc3b17acce71c5adf49286fd8fac0
juno-choi <ililil9482@naver.com>
2023-11-04 09:08:31 +0900

    start

---
+++ src/main/java/com/simol/reviewbot/ReviewBotApplication.java
@@ -0,0 +1,13 @@
+package com.simol.reviewbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewBotApplication.class, args);
+	}
+
+}
